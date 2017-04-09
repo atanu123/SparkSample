cc670fe906fb9c05c2c4d1cee2ebd3f9bb88ac25
atanu123 <atanu.chat@gmail.com>
2017-04-10 00:13:18 +0530

    changes done

--- src/main/scala/com/myprj/spark/HelloWorld.scala
+++ src/main/scala/com/myprj/spark/HelloWorld.scala
@@ -7,5 +7,6 @@ import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 object RunPrj {
   def main(args: Array[String]) = {
  println("**** Hi! I this Scala Program ******")
+ println("Hello World")
   }
 }
